f9fa4f542ed197e356a40d8638b8060af284f4c4
Paul Heading <hello@paulh.biz>
2018-12-24 01:01:16 +0000

    anim improvements, loop working

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,91 +1,93 @@
 
-var write1 = '.write:nth-child(1)', write2 = '.write:nth-child(2)', write3 = '.write:nth-child(3)',
-    write4 = '.write:nth-child(4)', write5 = '.write:nth-child(5)', write6 = '.write:nth-child(6)',
-    write7 = '.write:nth-child(7)', write8 = '.write:nth-child(8)', write9 = '.write:nth-child(9)',
-    write10 = '.write:nth-child(10)';
+var word1 = '.word:nth-child(1)', word2 = '.word:nth-child(2)', word3 = '.word:nth-child(3)',
+    word4 = '.word:nth-child(4)', word5 = '.word:nth-child(5)', word6 = '.word:nth-child(6)',
+    word7 = '.word:nth-child(7)', word8 = '.word:nth-child(8)', word9 = '.word:nth-child(9)',
+    word10 = '.word:nth-child(10)';
 
-var write = [write1,write2,write3,write4,write5,write6,write7,write8,write9,write10];
+var word = [word1,word2,word3,word4,word5,word6,word7,word8,word9,word10];
 
 function rowSwitch() {
 
   var rowSwitchTl = new TimelineMax({repeat:-1});
 
   rowSwitchTl
-  .staggerFromTo('.row',0.1,{backgroundColor:'transparent'},{backgroundColor:'#eee'},'1','0')
+  .staggerFromTo('.row',0.1,{backgroundColor:'transparent'},{backgroundColor:'rgba(0,0,0,0.3)'},'1','0')
   .staggerTo('.row',0.1,{backgroundColor:'transparent'},'1','1')
   ;
 
   return rowSwitch;
 }
 
-// function nyoType() {
-//
-//   var nyoTypeTl = new TimelineMax();
-//
-//   nyoTypeTl
-//   .set('body',{className:'layout-default _nyo'})
-//   .to(type1,1.0,{text:'WeMade'})
-//   .to(type2,1.0,{text:'a living identity'})
-//   .to(type3,0.5,{text:'for the'})
-//   .to(type4,0.5,{text:'NYO'})
-//   .to('.type',1.0,{text:'',onComplete:playType},'+=2')
-//   ;
-//
-//   nyoTypeTl.play(); return nyoType;
-// }
+function play() {
 
-// function playType() {
-//
-//   var playTypeTl = new TimelineMax({paused:true});
-//
-//   playTypeTl
-//   .set('body',{className:'layout-default _play'})
-//   .to(type1,1.0,{text:'WeMade'})
-//   .to(type2,1.0,{text:'a game'})
-//   .to(type3,0.5,{text:'for'})
-//   .to(type4,0.5,{text:'PLAY!'})
-//   .to('.type',1.0,{text:'',onComplete:nyoType},'+=2')
-//   ;
-//
-//   playTypeTl.play(); return playType;
-// }
+  var playTl = new TimelineMax();
 
-function heroBlob() {
-
-  var heroBlobTl = new TimelineMax();
-
-  heroBlobTl
+  playTl
   .set('#one',{opacity:0})
-  .set('#one',{opacity:1},'+=1')
-  .to('#one',0.6,{morphSVG:'#two',ease:SteppedEase.config(6)})
-  .set('#one',{morphSVG:'#three'},'+=0.2')
-  .set('#one',{morphSVG:'#four'},'+=0.2')
-  .set('#one',{morphSVG:'#five'},'+=0.2')
-  .set('#one',{morphSVG:'#six'},'+=0.6')
-  .set('#one',{morphSVG:'#seven'},'+=0.2')
-  .to('#one',0.6,{morphSVG:'#one',ease:SteppedEase.config(6)},'+=1.6')
+  // 0.4 seconds
+  .set('#one',{opacity:1},'0.4')
+  .to('#one',0.6,{morphSVG:'#two',ease:SteppedEase.config(6)},'0.4')
+  // 1.0 seconds
+  .set(word1,{text:'WeMade',fontWeight:'bold'},'1.0')
+  // 1.2 seconds
+  .set('#one',{morphSVG:'#three'},'1.2')
+  .set(word2,{text:'an'},'1.2')
+  // 1.4 seconds
+  .set('#one',{morphSVG:'#four'},'1.4')
+  .set(word3,{text:'8-bit'},'1.4')
+  // 1.6 seconds
+  .set('#one',{morphSVG:'#five'},'1.6')
+  .set(word4,{text:'world'},'1.6')
+  // 2.2 seconds
+  .set('#one',{morphSVG:'#six'},'2.2')
+  .set(word5,{text:'for'},'2.2')
+  // 2.4 seconds
+  .set('#one',{morphSVG:'#seven'},'2.4')
+  .set(word6,{text:'PLAY!',fontWeight:'bold'},'2.4')
+  // 4.4 seconds
+  .to('#one',0.6,{morphSVG:'#one',ease:SteppedEase.config(6)},'4.4')
+  .set(word,{text:'',clearProps:'all'},'4.4')
+  // 5.0 seconds
   .set('#one',{opacity:0})
   ;
 
-  return heroBlob;
+  return playTl;
 }
 
-function heroCopy() {
+function nyo() {
 
-  var heroCopyTl = new TimelineMax();
+  var nyoTl = new TimelineMax();
 
-  heroCopyTl
-  .set('body',{className:'layout-default _play'})
-  .set(write1,{text:'WeMade',fontWeight:'bold'},'+=1.6')
-  .set(write2,{text:'an'},'+=0.2')
-  .set(write3,{text:'8-bit'},'+=0.2')
-  .set(write4,{text:'world'},'+=0.2')
-  .set(write5,{text:'for'},'+=0.6')
-  .set(write6,{text:'PLAY!',fontWeight:'bold'},'+=0.2')
-  .set(write,{text:'',clearProps:'all'},'+=1.6')
+  nyoTl
+  .set('#one',{opacity:0})
+  // 0.4 seconds
+  .set('#one',{opacity:1},'0.4')
+  .to('#one',0.6,{morphSVG:'#two',ease:SteppedEase.config(6)},'0.4')
+  // 1.0 seconds
+  .set(word1,{text:'WeMade',fontWeight:'bold'},'1.0')
+  // 1.2 seconds
+  .set('#one',{morphSVG:'#three'},'1.2')
+  .set(word2,{text:'a'},'1.2')
+  // 1.4 seconds
+  .set('#one',{morphSVG:'#four'},'1.4')
+  .set(word3,{text:'living'},'1.4')
+  // 1.6 seconds
+  .set('#one',{morphSVG:'#five'},'1.6')
+  .set(word4,{text:'identity'},'1.6')
+  // 2.2 seconds
+  .set('#one',{morphSVG:'#six'},'2.2')
+  .set(word5,{text:'for'},'2.2')
+  // 2.4 seconds
+  .set('#one',{morphSVG:'#seven'},'2.4')
+  .set(word6,{text:'NYO',fontWeight:'bold'},'2.4')
+  // 4.4 seconds
+  .to('#one',0.6,{morphSVG:'#one',ease:SteppedEase.config(6)},'4.4')
+  .set(word,{text:'',clearProps:'all'},'4.4')
+  // 5.0 seconds
+  .set('#one',{opacity:0})
   ;
 
-  return heroCopy;
+  return nyoTl;
 }
 
 function heroSetup() {
@@ -93,14 +95,15 @@ function heroSetup() {
   var heroSetupTl = new TimelineMax({repeat:-1});
 
   heroSetupTl
-  .add(heroBlob())
-  .add(heroCopy())
+  .set('body',{className:'layout-default _play'})
+  .add(play())
+  .set('body',{className:'layout-default _nyo'})
+  .add(nyo())
   ;
 
 }
 
 $(document)
-.ready(rowSwitch)
+// .ready(rowSwitch)
 .ready(heroSetup)
-// .ready(nyoType)
 ;
