b7b94254c6df873c299a6796fced970cf6eb6f5b
Paul Heading <paul.heading@barbican.org.uk>
2019-05-11 23:53:29 +0100

    big text test

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -3,7 +3,7 @@ layout: none
 ---
 
 function getAjax(test) {
-  $.get('assets/svg/'+test+'.svg?v=1.3', function(data) {
+  $.get('assets/svg/'+test+'.svg?v=1.7', function(data) {
     var svg = new XMLSerializer().serializeToString(data.documentElement);
     $('.area._hero').html(svg);
   });
@@ -16,16 +16,16 @@ function getAjax(test) {
 function getAnim(test) {
   if(test == 'chloe') {
     var chloeTl = new TimelineMax({repeat:-1,yoyo:true});
-    chloeTl.to('#chloe',1,{x:50});
+    // chloeTl.to('#chloe',1,{x:50});
   }
   if(test == 'wemade') {
     var wemadeTl = new TimelineMax({repeat:-1,yoyo:true});
-    wemadeTl
-    .add('together')
-    .to('#face',1,{y:-100,rotation:360,transformOrigin:'center'},'together')
-    .to('#dollar',1,{y:100},'together')
-    .to('#dollar_1_',1,{x:300},'together')
-    .to('#dollar_2_',1,{x:-300},'together')
-    ;
+    // wemadeTl
+    // .add('together')
+    // .to('#face',1,{y:-100,rotation:360,transformOrigin:'center'},'together')
+    // .to('#dollar',1,{y:100},'together')
+    // .to('#dollar_1_',1,{x:300},'together')
+    // .to('#dollar_2_',1,{x:-300},'together')
+    // ;
   }
 }
